be41228499481e61d0560a163cafda89dd9a3a28
mosaebco <mosaebco.1380@gmail.com>
2023-10-14 23:18:16 +0330

    creating a layout for the first page

---
+++ resources/views/components/lays.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Jajiga Example</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resource/js/app.js'])
+
+    </head>
+    <body class="antialiased">
+        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-600 selection:bg-red-500 selection:text-white">
+            <div class="bg-gray-900">
+                <div class="sm:fixed sm:top-0 sm:left-0 p-6 text-left z-10">
+                    <h2 class="font-size-400 text-4xl font-semibold text-gray-200 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">JAJIGA</h2>
+                </div>
+                @if (Route::has('login'))
+                    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+    
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+            </div>
+
+            {{ $slot }}
+
+            <div class="bg-primary-300 text-primary-900 p-8">
+                <div class="p-16 container max-w-screen-xl mx-auto text-center">
+                    <a id="footer"></a>
+                    {{ $footer }}
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-lays>
+    <div>
+        <p class="bg-gray-500 rounde">Welcome to our wesite</p>
+    </div>
 
-        <title>Jajiga Example</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-    </head>
-    <body>
-        <div>
-            
-        </div>
-    </body>
-</html>
+    <x-slot name="footer">
+        All Rights Reserved &copy; {{ now()->year }}
+    </x-slot>
+</x-lays>
