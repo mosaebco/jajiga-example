05fa1becc2673a598a0595fd335ffe17e369ed7d
mosaebco <mosaebco.1380@gmail.com>
2023-10-25 14:06:53 +0330

    designig dashboaed view and adding rent button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -15,5 +15,9 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <div>
+        <a href="#" class="fixed right-6 bottom-6 text-gray-200 bg-primary-600 hover:bg-primary-700 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">Rent your place</a>
     </div>
 </x-app-layout>
